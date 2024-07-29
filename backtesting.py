14d5e5df776b4a9ea7a03f9f384f6865f84fd5ca
jurchiks33 <jurisslisans@gmail.com>
2024-07-29 20:51:17 +0200

    work on a backtesting file

---
+++ backtesting.py
@@ -0,0 +1,10 @@
+# Backtesting.py
+import pandas as pd
+
+def backtest(df):
+    initial_balance = 10000
+    balance = initial_balance
+    for index, row in df.iterrows():
+        # Trading logic is coming here
+        pass
+    return balance
