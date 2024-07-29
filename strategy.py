f3b66e42f6409b67f4fc53577a63163378da1e12
jurchiks33 <jurisslisans@gmail.com>
2024-07-29 20:46:03 +0200

    implementing strategy

--- strategy.py
+++ strategy.py
@@ -9,3 +9,9 @@ def calculate_signals(df):
     df['sell_signal'] = np.where(df['signal_condition'], -1, 0)
     return df
 
+def apply_stop_loss_take_profit(df, stop_loss_pct, take_profit_pct):
+    df['exit_signal'] = 0
+    # Here coming stop loss and take profit logic
+    return df
+
+if __name__ == "__main__":
