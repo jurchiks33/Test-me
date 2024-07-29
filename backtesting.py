8c481472c82d7650b9b942e90c044fb8031e9aff
jurchiks33 <jurisslisans@gmail.com>
2024-07-29 20:56:27 +0200

    backtesting file

--- backtesting.py
+++ backtesting.py
@@ -8,3 +8,8 @@ def backtest(df):
         # Trading logic is coming here
         pass
     return balance
+
+if __name__ == '__main__':
+    df = pd.read_csv('AAPL_data_with_signals.csv')
+    final_balance = backtest(df)
+    print(f"Final balance: ${final_balance}")
